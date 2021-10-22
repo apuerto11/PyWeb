3e88bacd970f017d45bad8adf45904c78406ac3c
Foetuus <lucasvincent69250@gmail.com>
2021-10-22 15:01:24 +0200

    Fonction insertion user

--- pyweb.py
+++ pyweb.py
@@ -8,15 +8,16 @@ from flask.cli import with_appcontext
 
 app = Flask(__name__)
 
-AppTitle= "PostIzi"
+AppTitle = "PostIzi"
 
-if not os.path.exists('instance'):
-    os.makedirs('instance')
+if not os.path.exists("instance"):
+    os.makedirs("instance")
+
 
 def get_db():
     db = sqlite3.connect(
-        os.path.join(app.instance_path, 'flaskr.sqlite'),
-        detect_types=sqlite3.PARSE_DECLTYPES
+        os.path.join(app.instance_path, "flaskr.sqlite"),
+        detect_types=sqlite3.PARSE_DECLTYPES,
     )
     db.row_factory = sqlite3.Row
 
@@ -26,16 +27,29 @@ def get_db():
 def init_db():
     db = get_db()
 
-    with app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
+    with app.open_resource("schema.sql") as f:
+        db.executescript(f.read().decode("utf8"))
 
-if not os.path.isfile('instance/flaskr.sqlite'):
+
+if not os.path.isfile("instance/flaskr.sqlite"):
     init_db()
+
 
 def hashMDP(pw):
     m = hashlib.sha256()
-    m.update(pw.encode('utf-8'))
+    m.update(pw.encode("utf-8"))
     return m.digest()
+
+
+def dbInsertUser(user, passw, firstname, name):
+    db = get_db()
+
+    db.execute(
+        "INSERT INTO users (username, password, firstname, name) VALUES (?, ?, ?, ?)",
+        (user, hashMDP(passw), name, firstname),
+    )
+    db.commit()
+
 
 # def login():
 #     error=None
@@ -45,27 +59,28 @@ def hashMDP(pw):
 #             return
 titre = "IziPost"
 
+
 @app.route("/")
 def index(name=None):
-    return render_template('index.html',title=titre)
+    return render_template("index.html", title=titre)
+
 
 @app.route("/about")
 def about():
-    return render_template('about.html',title=titre)
+    return render_template("about.html", title=titre)
+
 
 @app.route("/loginForm")
 def showLoginForm():
-   return render_template('loginForm.html',title=titre)
+    return render_template("loginForm.html", title=titre)
+
 
 @app.route("/signupForm")
 def showSignUpForm():
-   return render_template('signupForm.html',title=titre)
+    return render_template("signupForm.html", title=titre)
+
 
 @app.route("/dbisert")
-def insertDB():
-    db = get_db()
-
-    db.execute(
-        "INSERT INTO users (username, password, firstname, name) VALUES (?, ?, ?, ?)",
-        ("apuerto", hashMDP('password'), "Andrea", "Puerto"))
-    db.commit()
+def insertUser1():
+    dbInsertUser("user", "password", "firstname", "name")
+    return redirect(url_for("index"))
