261f941de7a9986b0ab0ba028c3c278ea6d1980e
Didjan <Didjan.sahnoun@hotmail.fr>
2021-10-22 10:36:22 +0200

    Modif 1

--- pyweb.py
+++ pyweb.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index(name=None):
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/loginForm")
+def showLoginForm():
+   return render_template('loginForm.html')
+
+@app.route("/signupForm")
+def showSignUpForm():
+   return render_template('signupForm.html')
